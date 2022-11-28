2b45666bdb39ad8ee0194e01d312fbf0e5bb5356
Matheus Toledo <matheus-mjtf-@hotmail.com>
2022-11-28 18:06:39 -0300

    toString method

--- lib/app/data/model/city.dart
+++ lib/app/data/model/city.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 class CityModel {
   int id;
   String name;
@@ -14,4 +15,7 @@ class CityModel {
         name: json['nome'],
         uf: json['estado']['uf'],
       );
+
+  @override
+  String toString() => 'id: $id, name: $name, uf: $uf';
 }
